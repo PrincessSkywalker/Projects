19071537c259f1cca0a6d2f4d0cbd488d222dcf1
Linan Qiu <linanqiu@gmail.com>
2015-09-29 00:32:49 -0400

    reformatted hw2 code

--- 2/src/Buffer.java
+++ 2/src/Buffer.java
@@ -1,25 +1,22 @@
 /** think of the cursor position as BETWEEN two adjacent characters.
-    let '|' show the cursor position. 
-    
-    
+    let '|' show the cursor position.
+
+
     cursor == 0 - cursor is between the begining of the buffer, and the first char - "|abcdef"
     cursor == 1 - "cursor is between the first and second char - "a|bcdef"
     cursor == 5 - cursor is between the 5th and 6th char - "abcde|f"
-    
-
 */
-interface Buffer 
-{
+interface Buffer {
 
   /** Get the current character count of the buffer  */
   int size();
-  
+
   /** load the buffer from an char array and position the cursor. after load
       size() == initial.length */
   void load(char initial[], int cursorPosition);
 
-  /** convert the current buffer contents to an array 
-   returnArray.length == size() 
+  /** convert the current buffer contents to an array
+   returnArray.length == size()
   */
   char[] toArray();
 
@@ -29,28 +26,28 @@ interface Buffer
   /** set the cursor position, in characters from start */
   void setCursor(int j);
 
-  /** move cursor one to the right 
+  /** move cursor one to the right
       "abc|def" => "abcd|ef"
   */
-  void moveRight();
+  void moveRight()
 
-  /** move cursor one to the left 
+  /** move cursor one to the left
       "abc|def" => "ab|cdef"
    */
-    void moveLeft();
+  void moveLeft()
 
-  /** insert a new char to the left of the cursor 
-   if the buffer is "abc|def", insertLeft('X') 
+  /** insert a new char to the left of the cursor
+   if the buffer is "abc|def", insertLeft('X')
    would change the buffer to 'abcX|def'
    */
-    void insertLeft(char c);
+  void insertLeft(char c);
 
-  /** delete and return the character to the right of the cursor 
+  /** delete and return the character to the right of the cursor
    given "abc|def", deleteRight() => "abc|ef"
    */
   char deleteRight();
 
-  /** delete and return the character to the left of the cursor 
+  /** delete and return the character to the left of the cursor
    given "abc|def", deleteLeft() => "ab|def"
    */
   char deleteLeft();
--- 2/src/CountOut.java
+++ 2/src/CountOut.java
@@ -1,24 +1,20 @@
-import java.util.*;
+import java.util.*
 
 public class CountOut {
 
-    public static List<Integer> play(int n, int k) {
-        
-        Queue<Integer> remainingPlayers = new LinkedList<>();        
-        
-        // Your code here.
-        return null; // replace this
-    }
+  public static List<Integer> play(int n, int k) {
 
-    
-    public static Integer findWinner(int n, int k) {
-        // Your code here.            
-        return null; // replace this
-    }
+    Queue<Integer> remainingPlayers = new LinkedList<>();
 
+    // Your code here.
 
-    public static Integer findWinnerRec(int n, int k) {
-        return null; // replace this
-    }
+  }
 
+  public static Integer findWinner(int n, int k) {
+    // Your code here.
+  }
+
+  public static Integer findWinnerRec(int n, int k) {
+
+  }
 }
