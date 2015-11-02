94fb2eb307a19b7f993dd5d500a6d4550801c403
Daniel Bauer <bauer@cs.columbia.edu>
2015-11-01 23:41:16 -0500

    added homework 4

---
+++ 4/src/AvlMap.java
@@ -0,0 +1,16 @@
+public class AvlMap<K extends Comparable<? super K>, V> implements Map<K, V> {
+
+  public AvlMap() {
+
+  }
+
+  @Override
+  public void put(K key, V value) {
+    return;
+  }
+
+  @Override
+  public V get(K key) {
+    return null;
+  }
+}
